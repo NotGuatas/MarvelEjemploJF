55c967c8d7663e0e2dd077a284a99343e5a2a026
NotGuatas <josephaugustoflores@gmail.com>
2025-01-18 08:58:21 -0500

    Agregar archivos de proyecto.

---
+++ MarvelEjemploJF/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MarvelEjemploJF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MarvelEjemploJF/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MarvelEjemploJF
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
